d73413f211b46b81f9a313d8cea792103fdedff1
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-08-19 17:44:28 -0700

    Complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+
+  def __init__(self, value=""):
+    self._value = value
+
+  @property
+  def value(self):
+    return self._value
+
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self._value[-1] == '.'
+  
+  def is_question(self):
+    return self._value[-1] == '?'
+  
+  def is_exclamation(self):
+    return self._value[-1] == '!'
+  
+  def count_sentences(self):
+    sum = 0
+    if(len(self._value) == 0):
+      return sum
+    else:
+      sentence_list = self.value.split(' ')
+    
+      for sentence in sentence_list:
+        if sentence[-1] == '.' or sentence[-1] == '?' or sentence[-1] == '!':
+          sum += 1
+        else:
+          sum += 0
+      return sum
